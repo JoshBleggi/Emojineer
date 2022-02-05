6ba50eb63991dacb019b3a5eb47def66e1f66762
Josh Bleggi <joshbleggi@yahoo.com>
2022-02-05 18:52:09 -0500

    Refactor response interaction into its own function

--- app.js
+++ app.js
@@ -14,56 +14,60 @@ app.command('/addemoji', async ({ command, payload, ack, respond }) => {
   await ack();
 
   console.log(payload);
+  await imageTooLarge(respond);
+  
+});
 
+async function imageTooLarge(respond) {
   await respond({
     blocks: [
-      {
-        "type": "section",
-        "text": {
-          "type": "plain_text",
-          "text": `Your image is too large for Slack to import automatically. Would you like to modify it?`
-        }
-      },
-      {
-        "type": "actions",
-        "elements": [
-          {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Resize"
-            },
-            "action_id": "resize"
-          },
-          {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Reduce Quality"
-            },
-            "action_id": "reduce_quality"
-          },
-          {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Crop"
-            },
-            "action_id": "crop"
-          },
-          {
-            "type": "button",
-            "text": {
-              "type": "plain_text",
-              "text": "Remove Frames"
-            },
-            "action_id": "remove_frames"
-          }
-        ]
-      }
-    ]
-  });
-});
+     {
+       "type": "section",
+       "text": {
+         "type": "plain_text",
+         "text": `Your image is too large for Slack to import automatically. Would you like to modify it?`
+       }
+     },
+     {
+       "type": "actions",
+       "elements": [
+         {
+           "type": "button",
+           "text": {
+             "type": "plain_text",
+             "text": "Resize"
+           },
+           "action_id": "resize"
+         },
+         {
+           "type": "button",
+           "text": {
+             "type": "plain_text",
+             "text": "Reduce Quality"
+           },
+           "action_id": "reduce_quality"
+         },
+         {
+           "type": "button",
+           "text": {
+             "type": "plain_text",
+             "text": "Crop"
+           },
+           "action_id": "crop"
+         },
+         {
+           "type": "button",
+           "text": {
+             "type": "plain_text",
+             "text": "Remove Frames"
+           },
+           "action_id": "remove_frames"
+         }
+       ]
+     }
+   ]
+ });
+}
 
 app.action('resize', async ({ body, ack, say }) => {
   await ack();
