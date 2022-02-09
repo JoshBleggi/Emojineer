0fcbbf2abddbe2a68d9d8d643dcae295f1c2d672
Josh Bleggi <joshbleggi@yahoo.com>
2022-02-08 19:00:46 -0500

    Wireframe Submit controller

---
+++ api/routes/submit.js
@@ -0,0 +1,8 @@
+const { App } = require('@slack/bolt');
+const express = require('express');
+
+function configureRoutes(app, api) {
+
+}
+
+module.exports = configureRoutes;
--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const { App } = require('@slack/bolt');
+const express = require('express');
 const appOptions = require('./options/appOptions.js');
 const subscribers = require('./subscribers');
+const submit = require('./api/routes/submit.js');
 
 // Initialize app with tokens
 const app = new App(appOptions.options);
 subscribers(app);
 
+const api = new express();
+submit(app, api);
+
 // Entrypoint into the app
 (async () => {
   // Start your app
-  await app.start(process.env.PORT || 3000);
+  await app.start(process.env.APP_PORT || 3000);
 
   console.log('🧪 Emojineer is running!');
+
+  api.listen(process.env.API_PORT || 3001, () => {
+    console.log('💻 API is active!');
+  });
 })();
