12c241de06e1d5e306b0ce33860f2edd801c43ef
Josh Bleggi <joshbleggi@yahoo.com>
2022-02-10 21:21:15 -0500

    Feat(Emoji): Add API and EmojiHandler service (#3)
    Revert "Feat(Emoji): Add API and EmojiHandler service (#3)" (#4)
    
    This reverts commit 06a32fd3fc34c30d193b2843b45f4551ff363347.
    
    Feat(Emoji): Add API and EmojiHandler service (#5)

---
+++ views/userImageEditingView.js
@@ -0,0 +1,51 @@
+function view(imageUrl, emojiName) {
+  var payload = `${imageUrl} ${emojiName}`;
+  return [
+     {
+       "type": "section",
+       "text": {
+         "type": "plain_text",
+         "text": "Your image is too large for Slack to import automatically. Would you like to modify it?"
+       }
+     },
+     {
+      "type": "image",
+      "image_url": imageUrl,
+      "alt_text": "Possible future Emoji"
+     },
+     {
+       "type": "actions",
+       "elements": [
+         {
+           "type": "button",
+           "text": {
+             "type": "plain_text",
+             "text": "Resize"
+           },
+           "action_id": "resize",
+           "value": payload
+         },
+         {
+           "type": "button",
+           "text": {
+             "type": "plain_text",
+             "text": "Crop"
+           },
+           "action_id": "crop",
+           "value": payload
+         },
+         {
+           "type": "button",
+           "text": {
+             "type": "plain_text",
+             "text": "Reduce Quality"
+           },
+           "action_id": "reduce_quality",
+           "value": payload
+         }
+       ]
+     }
+   ];
+}
+
+module.exports = { view };
